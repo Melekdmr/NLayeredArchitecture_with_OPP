2f6b3550f64a9a570b36d47d186a6f32e0877fe9
Melekdmr <melekdmrr01@gmail.com>
2024-10-16 23:39:00 +0300

    Add project files.

---
+++ DataAccessLayer/Baglanti.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace DataAccessLayer
+{
+	public class Baglanti
+	{
+		//Bazı durumlarda yeni bir nesne türetmeden kullanmamız gereken komutlar vardır.
+		//Bu komutlar statik olarak karşımaıza çıkar.
+		public static SqlConnection bgl = new SqlConnection(@"Data 
+                                         Source=MELEKDMR\\SQLEXPRESS;Initial 
+                                         Catalog=DbPersonel;Integrated Security=True;");
+
+	}
+}
